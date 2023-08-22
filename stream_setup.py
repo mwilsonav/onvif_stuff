2347eb7456dc41e0e7ca6802dd47ce8968e7ae6d
Mike Wilson <mwilson@altumvisio.com>
2023-08-22 11:49:52 -0600

    Improve the script to take command line arguments

--- stream_setup.py
+++ stream_setup.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python3
 
+import argparse
 from onvif import ONVIFCamera
 
-mycam = ONVIFCamera('192.168.2.11', 80, 'admin', '123456')
+def usage():
+    parser = argparse.ArgumentParser(description='Connect to an ONVIF camera and get the RTSP URI.')
+    parser.add_argument('hostname', help='Hostname or IP address of the camera.')
+    parser.add_argument('-p', '--port', default=80, type=int, help='Port number for the camera (default: 80).')
+    parser.add_argument('-u', '--username', default='admin', help='Username for the camera (default: admin).')
+    parser.add_argument('-w', '--password', default='123456', help='Password for the camera (default: 123456).')
+    return parser.parse_args()
 
-media_service = mycam.create_media_service()
-profiles = media_service.GetProfiles()
-token = profiles[0].token
+def main():
+    args = usage()
 
-obj = media_service.create_type('GetStreamUri')
-obj.ProfileToken = token
-obj.StreamSetup = {'Stream': 'RTP-Unicast', 'Transport': {'Protocol': 'RTSP'}}
-print(media_service.GetStreamUri(obj))
+    mycam = ONVIFCamera(args.hostname, args.port, args.username, args.password)
+
+    media_service = mycam.create_media_service()
+    profiles = media_service.GetProfiles()
+    token = profiles[0].token
+
+    obj = media_service.create_type('GetStreamUri')
+    obj.ProfileToken = token
+    obj.StreamSetup = {'Stream': 'RTP-Unicast', 'Transport': {'Protocol': 'RTSP'}}
+    print(media_service.GetStreamUri(obj))
+
+if __name__ == '__main__':
+    main()
+
